f1a62621967af9a177dd7f1758c2d4ba7a65d82d
Chayawit Wonganusorn <w.chayawit@gmail.com>
2024-08-09 14:06:48 +0000

    Add collections

---
+++ collections.py
@@ -0,0 +1,27 @@
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+# updating data
+myFruitList[2] = "orange"
+print(myFruitList)
+
+# Try tuples
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+# Try Dictionary
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
